f8b395d7d0cfebac1ef1e27f4b3ce67cc8d78edd
neha089 <nehapatel30325@gmail.com>
2024-03-22 18:28:58 +0530

    checking curds

--- BusReservation/src/main/java/javaproject/com/BusReservation/BusReservationApplication.java
+++ BusReservation/src/main/java/javaproject/com/BusReservation/BusReservationApplication.java
@@ -1,5 +1,6 @@
 package javaproject.com.BusReservation;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
